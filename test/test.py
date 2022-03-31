fd3362d2a082d502e88a1468b92c3f1092e23401
qiujiyan <33622081+qiujiyan@users.noreply.github.com>
2022-03-31 17:20:38 +0800

    Update test.py

--- test/test.py
+++ test/test.py
@@ -4,9 +4,3 @@ sys.path.append("c:/Users/macbook/githubProject/ofpp/")
 import Ofpp 
 workdir = r'C:/Users/macbook/githubProject/VortexTracking/data/case-oneCylinder'
 mesh = Ofpp.FoamMesh(workdir+'/')
-
-
-# a = "123\r\n321"
-# content.replace("\r\n",'\n')
-
-# print(a)
